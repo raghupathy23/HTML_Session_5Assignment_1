a4c497455d4b7c1e66ca40935c6ad1213d221f8e
raghupathy23 <raghupathy0123@gmail.com>
2016-10-02 19:12:22 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,13 @@
+	var name = "Raghu";
+	var age = 28;
+	var dateofbirth = "23 Jan 1988";
+	var placeofbirth = "COIMBATORE";
+	
+	// To display the values of each variable individually
+	console.log("Name: " +name);
+	console.log("Age: " +age);
+	console.log("Date of Birth: " +dateofbirth);
+	console.log("Place of Birth: " +placeofbirth);
+	
+	// To display the values in one line statement
+	console.log(name + " " + age + ", " + dateofbirth + " " + placeofbirth);
